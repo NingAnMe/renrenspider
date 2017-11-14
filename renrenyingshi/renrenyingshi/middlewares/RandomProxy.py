ad00ae53bf12836a849f66ca7b59d56f06d5c73b
NingAnMe <ninganme@qq.com>
2017-11-14 14:53:50 +0800

    添加IP隧道代理

--- renrenyingshi/renrenyingshi/middlewares/RandomProxy.py
+++ renrenyingshi/renrenyingshi/middlewares/RandomProxy.py
@@ -1,18 +1,21 @@
-#coding:utf-8
+# coding:utf-8
 import random
-
+import base64
 """
 这个类主要用于产生随机代理
 """
 class RandomProxy(object):
 
-
-    def __init__(self,http_proxy):#初始化一下数据库连接
+    def __init__(self, http_proxy, user_pass):  # 初始化一下数据库连接
         self.http_proxy = http_proxy
+        self.user_pass = user_pass
+        self.proxy_auth = self.proxy_authorization(self.user_pass)
 
     @classmethod
-    def from_crawler(cls,crawler):
-        return cls(crawler.settings.get('HTTP_PROXY'))
+    def from_crawler(cls, crawler):
+        return cls(crawler.settings.getlist('HTTP_PROXY'),
+                   crawler.settings.get('PROXY_USER_PASSWOED'))
+
 
     def process_request(self, request, spider):
         """
@@ -21,4 +24,18 @@ class RandomProxy(object):
         :param spider:
         :return:
         """
-        request.meta['proxy'] =self.http_proxy
+        # 使用普通代理
+        # p = random.choice(self.proxy_list)
+        # proxy = r'http://%s:%s' %(p[0],p[1])
+        # request.meta['proxy'] = proxy
+
+        # 使用隧道代理
+        proxy = "http://http-dyn.abuyun.com:9020"
+        request.meta['proxy'] = proxy
+        request.headers["Proxy-Authorization"] = self.proxy_auth
+
+    def proxy_authorization(self, user_pass):
+        """产生隧道代理的Proxy-Authorization
+        """
+        proxy_auth = 'Basic %s' % base64.b64encode(user_pass)
+        return proxy_auth
--- renrenyingshi/renrenyingshi/middlewares/RandomUserAgent.py
+++ renrenyingshi/renrenyingshi/middlewares/RandomUserAgent.py
@@ -1,18 +1,19 @@
-#coding:utf-8
+# coding:utf-8
 import random
 
 """
 这个类主要用于产生随机UserAgent
 """
 
+
 class RandomUserAgent(object):
 
-    def __init__(self,agents):
+    def __init__(self, agents):
         self.agents = agents
 
     @classmethod
-    def from_crawler(cls,crawler):
+    def from_crawler(cls, crawler):
         return cls(crawler.settings.getlist('USER_AGENTS'))
 
-    def process_request(self,request,spider):
+    def process_request(self, request, spider):
         request.headers.setdefault('User-Agent', random.choice(self.agents))
